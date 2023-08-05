a49a69e1ee2ee7f50c931356745a8b9565a02f7d
saitejachandala <104210102+saitejachandala@users.noreply.github.com>
2023-08-05 20:33:33 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,178 @@
+import Gregg from "../src/assets/images/gregg.png"
+import NLHA from "../src/assets/images/nlha.png"
+import WCA from "../src/assets/images/wca.png"
+import MKJ from "../src/assets/images/mkj.png"
+import PPA from "../src/assets/images/ppa.png"
+import FM from "../src/assets/images/fm.png"
+import BCA from "../src/assets/images/BCA.png"
+import BLG from "../src/assets/images/BLG.png"
+import MS from "../src/assets/images/ms.jpeg"
+import TC from "../src/assets/images/TC.png"
+import SM from "../src/assets/images/sm.jpeg"
+import Profile from "../src/assets/images/profile.png"
+import BAP from "../src/assets/images/bap.jpeg"
+const logotext = "Divya Narsingh";
+const meta = {
+    title: "Narsingh Divya",
+    description: "I’m Divya _ Web Designer,currently working in Hyderabad",
+};
+
+const introdata = {
+    title: "I’m Divya",
+    animated: {
+        first: "I love Designing",
+        second: "I do cool websites",
+        third: "I will design Websites",
+    },
+    description: "Passionate about web designing, I strive to create visually stunning and user-friendly websites that leave a lasting impression. With a keen eye for aesthetics and a love for solving design challenges, I am dedicated to crafting digital experiences that elevate brands and engage audiences. Web designing is not just a profession but a creative outlet that fuels my passion for innovation and continuous learning.",
+    your_img_url: Profile,
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "A passionate web designer with sweet spot for creating visually enchanting user centric digital designs. Design has always been my profound interest. This 5 years of experience helped me too hone my skills to blend creativity and design seamlessly. For me, Web designing is not just pertained to design, but to create an everlasting impression for the customer.  ",
+};
+const worktimeline = [{
+        jobtitle: "Web Designer",
+        where: "NICE Digitals",
+        date: "2017 April - 2020 Sept",
+    },
+    {
+        jobtitle: "Web Designer",
+        where: "SHV Technologies",
+        date: "2021 August - Till Now",
+    },
+];
+
+const skills = [{
+        name: "Adobe Illustration",
+        value: 90,
+    },
+    {
+        name: "Canva",
+        value: 85,
+    },
+    {
+        name: "Photoshop",
+        value: 95,
+    },
+    // {
+    //     name: "React",
+    //     value: 60,
+    // },
+    // {
+    //     name: "Jquery",
+    //     value: 85,
+    // },
+];
+
+const services = [{
+        title: "Graphic Design",
+        description: "Creates illustartion as per the requirement that catches the eye of the customer and that communicates their brands message. .",
+    },
+    {
+        title: "Branding",
+        description: "I develop a brand narrative that inculcates the identity and pereception of the business into the digital illustartion.",
+    },
+    {
+        title: "Logo Design",
+        description: "I take the pride and craft logos that are visually appealing and deeply meaningful. I intend to create ever-longlasting impression on my customer, As logo is not just symbol but it inculcates the values and aspirations of the brand.",
+    },
+    {
+        title: "Truck Wrap",
+        description: "Smallest icon to largest truck wrap, i create a harmonius visual language thats explicit and contains a compelling story through design .",
+    },
+];
+
+const dataportfolio = [{
+        img: Gregg,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://greggcustompainting.com/",
+    },
+    {
+        img: NLHA,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.newlifehouseacademy.org/",
+    },
+    {
+        img: WCA,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.wolfcreekacademy.org/",
+    },
+    {
+        img: MKJ,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://mckibbenjohnson.com/",
+    },
+    {
+        img: PPA,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://providencepass.com/",
+    },
+    {
+        img: FM,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://helpmyboy.com/",
+    },
+
+    {
+        img: BCA,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.brushcreekacademy.com/",
+    },
+    {
+        img: BLG,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://burgesslawgroup.com/",
+    },
+    {
+        img: MS,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.mainstreethvac.com/",
+    },
+    {
+        img: TC,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://treasurecoastacademy.com/",
+    },
+    {
+        img: SM,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: BAP,
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://www.bapheating.ca/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "divya.narsingh@gmail.com",
+    YOUR_FONE: "7799378723",
+    //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    // github: "https://github.com",
+    // facebook: "https://facebook.com",
+     linkedin: "https://www.linkedin.com/in/divya-narsingh-b73572279/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
